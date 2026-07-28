99faba57283bf78ff25e1714c3d72d6f2f49b728
Salma Arbaoui <salma.arbaoui15@gmail.com>
2026-07-28 09:28:10 +0200

    modif

--- collect.py
+++ collect.py
@@ -1,24 +1,24 @@
 import duckdb
 import pandas as pd 
 
-
-def collect(url_StockEtablissement,url_Historique,departement):
-     con = duckdb.connect()
+con = duckdb.connect()
+def collect(con,url_StockEtablissement,url_Historique,departement):
+     #con = duckdb.connect()
      con.execute("INSTALL httpfs; LOAD httpfs;")
      resultat1=con.sql(f"""
     SELECT siret, 
-           codeCommuneEtablissement as code_commune,
+                      codeCommuneEtablissement as code_commune,
                        trancheEffectifsEtablissement as tranche_effectifs,
                       libelleCommuneEtablissement as libelle_commune,
                       activitePrincipaleEtablissement as code_ape,
-                      nomenclatureActivitePrincipaleEtablissement as nomenclature_ape
+                      nomenclatureActivitePrincipaleEtablissement as nomenclature
              FROM read_parquet('{url_StockEtablissement}')
                       WHERE substr(codeCommuneEtablissement, 1, 2) = '{departement}'   
                       AND statutDiffusionEtablissement = 'O'              
         """)
      
      resultat2=con.sql(f"""
-         SELECT ss.siret,ss.dateFin,ss.dateDebut , ss.etatAdministratifEtablissement    AS etat, ss.activitePrincipaleEtablissement  
+         SELECT ss.siret,ss.dateFin,ss.dateDebut , ss.etatAdministratifEtablissement   AS etat, ss.activitePrincipaleEtablissement as code_ape 
           FROM read_parquet('{url_Historique}') ss
           
          WHERE ss.siret IN (SELECT siret FROM resultat1)
