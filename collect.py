19a7455d79eff21d09b0d7fdf13981d4df63afe7
merazig <mohamed7gp@gmail.com>
2026-07-30 16:10:07 +0200

    brief3 collect_alt

--- collect.py
+++ collect.py
@@ -27,9 +27,24 @@ def collect(con, url_StockEtablissement, url_Historique, departement):
      """) 
      return (resultat1, resultat2)
 
-
-
-
-
-
-
+def collect_alt(con, url_StockEtablissement, url_Historique, departement):
+  # elle retourne une seule table
+  duckDBcursor = con.execute(f"""
+                      SELECT
+                        s.siret,
+                        s.codeCommuneEtablissement AS code_commune,
+                        s.trancheEffectifsEtablissement AS tranche_effectifs,
+                        s.libelleCommuneEtablissement AS libelle_commune,
+                        h.dateDebut,
+                        h.dateFin,
+                        h.etatAdministratifEtablissement AS etat,
+                        h.activitePrincipaleEtablissement AS code_ape,
+                        h.nomenclatureActivitePrincipaleEtablissement AS nomenclature_ape
+                    FROM read_parquet('{url_StockEtablissement}') AS s
+                    LEFT JOIN read_parquet('{url_Historique}') AS h
+                        ON s.siret = h.siret
+                    WHERE substr(s.codeCommuneEtablissement, 1, 2) = '{departement}' 
+                        AND s.statutDiffusionEtablissement = 'O'
+                    ORDER BY s.siret, h.dateDebut;
+                """)
+  return duckDBcursor
